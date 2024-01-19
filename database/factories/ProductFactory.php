96a417ff4fa091f4d210cf5502ca13c244d07d1c
Fawwaz Bayureksa <fawwazbayureksa@gmail.com>
2024-01-19 10:21:38 +0700

    set up database and factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_name' => $this->faker->word,
+            'image' => $this->faker->imageUrl(),
+            'qty' => $this->faker->randomNumber(2),
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'categories' => $this->faker->word,
+        ];
+    }
+}
--- database/migrations/2024_01_19_022234_create_logs_table.php
+++ database/migrations/2024_01_19_022234_create_logs_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->integer('user_id');
             $table->integer('product_id');
-            $table->integer('product_id');
+            $table->string('route');
+            $table->string('message');
             $table->timestamps();
         });
     }
